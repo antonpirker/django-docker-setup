84edc765727ab1353a96faf13b70348a4536bd8b
Anton Pirker <anton@ignaz.at>
2019-09-30 14:19:38 +0200

    bla

--- maintainer/core/views.py
+++ maintainer/core/views.py
@@ -5,19 +5,8 @@ from django.template.loader import render_to_string
 def index(request):
     context = {
         'projects': Project.objects.all().order_by('name'),
-        'nochwas': None,
-        'und noch': None,
-        'bla': None,
     }
-
-    # Das könnte man besser machen.
 
     rendered = render_to_string('index.html', context=context)
     return HttpResponse(rendered)
 
-
-
-def bla(request):
-    #das ist auch was neues
-
-    return HttpResponse('oK')
